e008eb155447b99ebdbc4ed2721f712edb7c3b21
pauloricardolongi <pauloricardolongi@gmail.com>
2022-01-08 02:13:39 -0300

    com expressão lambda declarada

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -3,6 +3,7 @@ package aplicacao;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import entidades.Product;
 
@@ -18,7 +19,12 @@ public class Programa {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(Product::nonStaticPriceUpdate);
+		double factor = 1.1;
+		Consumer<Product> cons = p -> p.setPreco(p.getPreco() * factor);
+			
+		
+		
+		list.forEach(cons);
 		
 		list.forEach(System.out::println);
 
